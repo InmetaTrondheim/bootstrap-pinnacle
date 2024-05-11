e589ba0f2b6eb0b82649e4656efa0818782b6ce6
marius <marnyg@proton.me>
2024-05-11 15:05:54 +0000

    fix: adding earthfile

--- modules/azure_devops_project/main.tf
+++ modules/azure_devops_project/main.tf
@@ -93,6 +93,47 @@ steps:
   commit_message      = "pipeline"
   overwrite_on_create = false
 }
+resource "azuredevops_git_repository_file" "earthfile" {
+  for_each            = azuredevops_git_repository.template_repo
+  depends_on          = [null_resource.push_repo, ]
+
+  repository_id       = each.value.id
+  file                = "Earthfile"
+  content             = <<EOF
+# Earthfile
+
+# Base setup with all necessary dependencies
+VERSION 0.8
+FROM earthly/earthly:latest
+WORKDIR /code
+COPY . .
+
+# Build target
+build:
+    RUN echo running \"BUILD  +build\"
+
+# Immediate tests target
+immediate-tests:
+    RUN echo running \"BUILD  +unit\"
+    RUN echo running \"BUILD  +scan-secrets\"
+
+# Publish target
+publish:
+    RUN echo running \"BUILD  +scan-secrets\"
+
+# Comprehensive tests target
+comprehensive-tests:
+    RUN echo running \"BUILD  +integration-test\"
+    RUN echo running \"BUILD  +e2e-test\"
+
+# Deployment target
+deploy:
+    RUN echo running \"BUILD  +deploy\"
+EOF
+  branch              = "refs/heads/main"
+  commit_message      = "earthfile"
+  overwrite_on_create = false
+}
 
 
 
