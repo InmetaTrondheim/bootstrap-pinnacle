0f2fe8573aa79bf04ef0981c0befea6a499227c6
marius <marnyg@proton.me>
2024-05-13 09:26:07 +0000

    chore: cleanuop

--- main.tf
+++ main.tf
@@ -7,36 +7,3 @@ module "azure_devops_project" {
   project_name = "tfCreatedPro3"
 }
 
-
-
-
-
-
-
-# module "repo_creation" {
-#   source       = "github.com/InmetaTrondheim/bootstrap-pinnacle//modules/repo_creation"
-#   bootstrap_script = "docker run -v $(pwd):/out ghcr.io/inmetatrondheim/dotnet-template:main -n tset "
-#   repo_host        = "azuredevops"
-# }
-# module "azure_devops_project" {
-#   source       = "github.com/InmetaTrondheim/bootstrap-pinnacle//modules/azure_devops_project"
-#   project_name = "tfCreatedPro3"
-#   template_repos = {
-#     "frontend" = {
-#       repo_name  = "frontend",
-#       init_type  = "Import",
-#       source_url = "https://github.com/InmetaTrondheim/nextjs-template.git"
-#     },
-#     "backend" = {
-#       repo_name            = "backend",
-#       init_type            = "Uninitialized",
-#       template_folder_path = module.repo_creation.initialized_folder_path
-#     },
-#     "infra" = {
-#       repo_name  = "infra",
-#       init_type  = "Import",
-#       source_url = "https://github.com/InmetaTrondheim/terraform-azure.git"
-#     }
-#   }
-# }
-#
